05f606d1f8c2c4a35cce02e0719ac55baaf8e078
Agatasil <126770043+Agatasil@users.noreply.github.com>
2023-03-12 12:03:40 +0100

    Add files via upload
    zadanie dzien 4

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string name = "Ewa";
+string sex = "kobieta";
+var age = 50;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniezej 30 lat");
+}
+else if (sex == "kobieta" && name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "mezczyzna" && age < 18)
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
+else if (sex == "mezczyzna" && age >= 18)
+{
+    Console.WriteLine("pelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("Inna kobieta");
+}
